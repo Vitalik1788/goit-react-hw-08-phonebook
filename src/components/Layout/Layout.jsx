79be4cdd3c015504d90600d8463fb81387f90cd3
Vitalik1788 <tarasenkovitalik2013@ukr.net>
2023-06-18 18:23:54 +0300

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,4 @@
 import { Suspense } from 'react';
-import { HeaderList, LinkStyle, RegContainer } from './Layout.styled';
 import { Outlet } from 'react-router-dom';
 import Spiner from 'components/Spinner/Spinner';
 import { AppBar } from 'components/AppBar/AppBar';
